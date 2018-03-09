de14e778aed5d841d968bcfc07e38b34ceb19d1f
Vadim Yanko <yanko.vadim@gmail.com>
2018-03-09 19:49:39 +0200

    Hello world

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject ad-astra "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :main ^:skip-aot ad-astra.core
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [ring/ring-core "1.6.3"]
+                 [ring/ring-jetty-adapter "1.6.3"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
