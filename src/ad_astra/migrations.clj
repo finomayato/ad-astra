697e229a23cf1e07071a2bad0e0e696cc0d922f7
Vadim Yanko <yanko.vadim@gmail.com>
2018-03-18 00:19:48 +0200

    Created goals and users tables

--- src/ad_astra/migrations.clj
+++ src/ad_astra/migrations.clj
@@ -1,5 +1,24 @@
 (ns ad-astra.migrations
   (:require [clojure.java.jdbc :as jdbc]))
 
+
+(def users-table-ddl
+  (jdbc/create-table-ddl :users
+                         [:id :serial "PRIMARY KEY"]
+                         [:name "varchar(255)" "NOT NULL"]
+                         [:email "varchar(255)" "NOT NULL"]))
+
+(def goals-table-ddl
+  (jdbc/create-table-ddl :goals
+                         [:id :serial "PRIMARY KEY"]
+                         [:name "varchar(255)" "NOT NULL"]
+                         [:start_date "DATE"]
+                         [:end_date "DATE"]
+                         [:parent_goal_id :int "references goals (id)"]
+                         [:user_id :int "references users (id)"]
+                         [:description :text]
+                         [:is_private :bool "default true"]))
+
 (def migration-map
-  {})
+  {:1 users-table-ddl
+   :2 goals-table-ddl})
