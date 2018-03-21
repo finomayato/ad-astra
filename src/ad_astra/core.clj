70f496d9b1cd48b272ed75e354131a20c5e43cf9
Vadim Yanko <yanko.vadim@gmail.com>
2018-03-21 23:11:12 +0200

    Added wrap-params

--- src/ad_astra/core.clj
+++ src/ad_astra/core.clj
@@ -1,6 +1,10 @@
-(ns ad-astra.core)
+(ns ad-astra.core
+  (:require [ring.middleware.params :refer [wrap-params]]))
 
 (defn handler [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body "Hello World"})
+
+(def app
+  (-> handler wrap-params))
