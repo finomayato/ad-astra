981a0390f746f88b8cd0be0d91a7aba1618ab9e5
Vadim Yanko <yanko.vadim@gmail.com>
2018-04-11 01:34:04 +0300

    Methods map was created

--- src/ad_astra/core.clj
+++ src/ad_astra/core.clj
@@ -3,10 +3,29 @@
                                           wrap-json-response]])
   (:require [ring.util.response :refer [response]]))
 
+
+(defn handle-get [request]
+  (when (= "/" (:uri request))
+    (response "This is a home page!")))
+
+(defn handle-post [request]
+  (response "POST"))
+
+(defn handle-put [request]
+  (response "PUT"))
+
+(defn handle-del [request]
+  (response "DEL"))
+
+(def method-map {:get handle-get
+                 :post handle-post
+                 :put handle-put
+                 :del handle-del})
+
 (defn handler [request]
-  (prn request)
-  (when (= :get (:request-method request))
-    (response {"name" "Vadim"})))
+  (let [request-method (:request-method request)]
+    ((get method-map request-method) request)))
+  
 
 (def app
   (-> handler
